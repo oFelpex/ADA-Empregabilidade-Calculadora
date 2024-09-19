1a5e6f2f48e2bf064d17c40aca9ee8e9d0cc220b
Felipe Costa da Silva <felipe95176@gmail.com>
2024-09-19 11:40:40 -0300

    Organiza os botões de números, mas ainda sem estilização.

--- dist/main.js
+++ dist/main.js
@@ -3,7 +3,7 @@ function createButtons() {
     const buttonsContainer = document.getElementById("buttons");
     if (!buttonsContainer)
         return console.error("Buttons container not found");
-    for (let i = 9; i >= 1; i--) {
+    for (let i = 1; i <= 9; i++) {
         const button = document.createElement("button");
         button.id = `button-${i}`;
         button.textContent = i.toString();
@@ -11,6 +11,11 @@ function createButtons() {
             console.log(`Button ${i} clicked!`);
         });
         buttonsContainer.appendChild(button);
+        button.style.gridRow = "4";
+        if (i > 3)
+            button.style.gridRow = "3";
+        if (i > 6)
+            button.style.gridRow = "2";
     }
     ;
 }
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 function createButtons() {
     const buttonsContainer = document.getElementById("buttons");
     if (!buttonsContainer) return console.error("Buttons container not found");
-    for (let i: number = 9; i >= 1; i--) {
+    for (let i: number = 1; i <= 9; i++) {
         const button = document.createElement("button");
         button.id = `button-${i}`;
         button.textContent = i.toString();
@@ -9,6 +9,11 @@ function createButtons() {
             console.log(`Button ${i} clicked!`);
         });
         buttonsContainer.appendChild(button);
+        button.style.gridRow = "4";
+        if(i > 3)
+            button.style.gridRow = "3";
+        if(i > 6)
+            button.style.gridRow = "2";
     };
 };
 createButtons();
