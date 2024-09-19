1ccb38776035bec09d64e72720a54f072fd82a8e
Felipe Costa da Silva <felipe95176@gmail.com>
2024-09-19 11:55:44 -0300

    Muda o nome da variável "button" para "n_button", a fim de melhorar a legibilidade do código, também adiciona o botão de número ZERO. Adiciona um novo botão com a id "button-c", também começa a estilizá-lo no CSS. Altera o tamanho dos botões e da fonte de texto.

--- dist/main.js
+++ dist/main.js
@@ -3,21 +3,27 @@ function createButtons() {
     const buttonsContainer = document.getElementById("buttons");
     if (!buttonsContainer)
         return console.error("Buttons container not found");
-    for (let i = 1; i <= 9; i++) {
-        const button = document.createElement("button");
-        button.id = `button-${i}`;
-        button.textContent = i.toString();
-        button.addEventListener("click", () => {
+    for (let i = 0; i <= 9; i++) {
+        const n_button = document.createElement("button");
+        n_button.id = `button-${i}`;
+        n_button.textContent = i.toString();
+        n_button.addEventListener("click", () => {
             console.log(`Button ${i} clicked!`);
         });
-        buttonsContainer.appendChild(button);
-        button.style.gridRow = "4";
+        buttonsContainer.appendChild(n_button);
+        n_button.style.gridRow = "5";
+        if (i > 0)
+            n_button.style.gridRow = "4";
         if (i > 3)
-            button.style.gridRow = "3";
+            n_button.style.gridRow = "3";
         if (i > 6)
-            button.style.gridRow = "2";
+            n_button.style.gridRow = "2";
     }
     ;
+    const clearButton = document.createElement("button");
+    clearButton.id = "button-c";
+    clearButton.textContent = "C";
+    buttonsContainer.appendChild(clearButton);
 }
 ;
 createButtons();
