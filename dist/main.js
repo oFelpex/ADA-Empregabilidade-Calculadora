f4a0addd7b92cdd266928f27e195ecc548954afc
Felipe Costa da Silva <felipe95176@gmail.com>
2024-09-19 11:32:20 -0300

    Cria o começo da função que irá gerar os botões. Adiciona os arquivos "tsconfig.json" (necessário para a configuração do .ts) e main.js que é gerado a partir do main.ts.

--- dist/main.js
+++ dist/main.js
@@ -1,7 +1,9 @@
+"use strict";
 function createButtons() {
     const buttonsContainer = document.getElementById("buttons");
-    if (!buttonsContainer) return console.error("Buttons container not found");
-    for (let i: number = 9; i >= 1; i--) {
+    if (!buttonsContainer)
+        return console.error("Buttons container not found");
+    for (let i = 9; i >= 1; i--) {
         const button = document.createElement("button");
         button.id = `button-${i}`;
         button.textContent = i.toString();
@@ -9,6 +11,8 @@ function createButtons() {
             console.log(`Button ${i} clicked!`);
         });
         buttonsContainer.appendChild(button);
-    };
-};
+    }
+    ;
+}
+;
 createButtons();
