de0cc13cbeae6dd907fd0ecf44257e301f8b9b42
Felipe Costa <felipe95176@gmail.com>
2024-09-23 18:16:03 -0300

    Muda o type do display para text, e adiciona alguns eventos para funcionar os botões da calculadora

--- dist/main.js
+++ dist/main.js
@@ -7,6 +7,7 @@ function createDisplay() {
         if (display.value.length > 15) {
             display.value = display.value.slice(0, 15);
         }
+        display.value = display.value.replace(/[^0-9,.%+-/]/g, "");
     });
     display.addEventListener("focusin", () => {
         if (display.value === "0") {
@@ -18,15 +19,17 @@ function createDisplay() {
             display.value = "0";
         }
     });
+    return display;
 }
 function createButtons() {
-    createDisplay();
+    let display = createDisplay();
     const buttonsContainer = document.getElementById("buttons");
     if (!buttonsContainer)
         return console.error("Buttons container not found");
     for (let i = 0; i <= 9; i++) {
         const n_button = document.createElement("button");
         n_button.id = `button-${i}`;
+        n_button.classList.add("displayButton");
         n_button.textContent = i.toString();
         buttonsContainer.appendChild(n_button);
         n_button.style.gridRow = "5";
@@ -41,16 +44,19 @@ function createButtons() {
     const clearButton = document.createElement("button");
     clearButton.id = "button-c";
     clearButton.textContent = "C";
+    clearButton.addEventListener("click", () => display.value = "0");
     buttonsContainer.appendChild(clearButton);
     const commaButton = document.createElement("button");
     commaButton.id = "button-comma";
     commaButton.textContent = ",";
     commaButton.style.gridRow = "5";
+    commaButton.classList.add("displayButton");
     buttonsContainer.appendChild(commaButton);
     const percentButton = document.createElement("button");
     percentButton.id = "button-percent";
     percentButton.textContent = "%";
     percentButton.style.gridRow = "5";
+    percentButton.classList.add("displayButton");
     buttonsContainer.appendChild(percentButton);
     const sqrtButton = document.createElement("button");
     sqrtButton.id = "button-sqrt";
@@ -59,29 +65,45 @@ function createButtons() {
     buttonsContainer.appendChild(sqrtButton);
     const multiplyButton = document.createElement("button");
     multiplyButton.id = "button-multiply";
-    multiplyButton.textContent = "X";
+    multiplyButton.textContent = "*";
     multiplyButton.style.gridRow = "1";
+    multiplyButton.classList.add("displayButton");
     buttonsContainer.appendChild(multiplyButton);
     const divisionButton = document.createElement("button");
     divisionButton.id = "button-division";
     divisionButton.textContent = "÷";
     divisionButton.style.gridRow = "1";
+    divisionButton.classList.add("displayButton");
     buttonsContainer.appendChild(divisionButton);
     const minusButton = document.createElement("button");
     minusButton.id = "button-minus";
     minusButton.textContent = "-";
     minusButton.style.gridRow = "2";
+    minusButton.classList.add("displayButton");
     buttonsContainer.appendChild(minusButton);
     const plusButton = document.createElement("button");
     plusButton.id = "button-plus";
     plusButton.textContent = "+";
     plusButton.style.gridRow = "3";
+    plusButton.classList.add("displayButton");
     buttonsContainer.appendChild(plusButton);
     const equalButton = document.createElement("button");
     equalButton.id = "button-equal";
     equalButton.textContent = "=";
     equalButton.style.gridRow = "4 / span 2";
     buttonsContainer.appendChild(equalButton);
+    function showNumbers() {
+        let buttons = Array.from(document.getElementsByClassName("displayButton"));
+        buttons.forEach((element) => {
+            element.addEventListener("click", () => {
+                if (display.value === "0") {
+                    display.value = "";
+                }
+                display.value += element.innerHTML;
+            });
+        });
+    }
+    showNumbers();
 }
 ;
 createButtons();
